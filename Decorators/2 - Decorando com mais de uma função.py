a464a93b1e097ddf90738ba7743164442794c9a7
Emerson Rafael <emersona7x@hotmail.com>
2022-12-28 01:31:03 -0300

    [CHG] EXEMPLO DECORANDO COM MAIS DE UMA FUNÇÃO, USANDO CACHE E O MEDIDOR DE TEMPO

--- Decorators/2 - Decorando com mais de uma função.py
+++ Decorators/2 - Decorando com mais de uma função.py
@@ -1,14 +1,19 @@
+from functools import cache
 from time import sleep
 
 from medidor_de_tempo import medidor_de_tempo
 
 
+@cache
 @medidor_de_tempo
 def delay(secs):
 
     """
 
         A FUNÇÃO REALIZA O SLEEP DE X SECS.
+
+        PODEMOS DECORAR COM MAIS DE UMA FUNÇÃO
+        NESSE CASO, ALÉM DE MEDIR O TEMPO, TEMOS A FUNÇÃO DE CACHÊ
 
     """
 
@@ -19,4 +24,5 @@ def delay(secs):
 
 if __name__ == '__main__':
 
-    delay(3)
+    print(delay(5), delay(5), delay(5))
+
